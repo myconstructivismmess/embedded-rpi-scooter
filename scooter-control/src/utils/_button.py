7d78df6484b78bf389a2f4ad67914b1285dd6b3c
Aubrey ROUET <aubrey.rouet@gmail.com>
2024-07-27 18:41:52 +0200

    Remove create_base_button_builder and add add_debug_interceptor_if_debug

--- scooter-control/src/utils/_button.py
+++ scooter-control/src/utils/_button.py
@@ -5,12 +5,6 @@ from microcontroller import Pin
 from Button import ButtonBuilder
 
 # Main
-def create_base_button_builder(gpio_pin: Pin, debug_key_name: str, debug: bool = False) -> ButtonBuilder:
-    builder = ButtonBuilder()
-
-    builder.add_gpio_pin_interceptor(gpio_pin)
-    
+def add_debug_interceptor_if_debug(builder: ButtonBuilder, debug_key_name: str, debug: bool = False) -> None:    
     if debug:
         builder.add_keyboard_interceptor(debug_key_name)
-
-    return builder
