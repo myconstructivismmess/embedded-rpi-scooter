02c00290da68ba8924de6ddef91c8d3dbb22924f
Aubrey ROUET <aubrey.rouet@gmail.com>
2024-07-28 15:22:47 +0200

    Update LatchButtonHandler to get and set value.

--- scooter-control/src/Button/_ButtonHandlers/_LatchButtonHandler.py
+++ scooter-control/src/Button/_ButtonHandlers/_LatchButtonHandler.py
@@ -14,11 +14,19 @@ class LatchButtonHandler(ButtonHandlerBase):
 
     def on_value_raised(self) -> None:
         if self._enabled:
-            self._value = not self._value
-            self._on_value_changed(self._value)
-
+            self.set_value(not self._value)
     def on_value_recessed(self) -> None:
         pass
 
     def update(self) -> None:
         pass
+
+    @property
+    def value(self) -> bool:
+        return self.get_value()
+    def get_value(self) -> bool:
+        return self._value
+    def set_value(self, value: bool, trigger_on_value_change_callback: bool = True) -> None:
+        self._value = value
+        if trigger_on_value_change_callback:
+            self._on_value_changed(self._value)
