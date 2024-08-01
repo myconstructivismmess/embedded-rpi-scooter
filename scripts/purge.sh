597f35d7b9dd125a7bbf8b2eb6663e202e82b879
Aubrey ROUET <aubrey.rouet@gmail.com>
2024-08-02 00:44:39 +0200

    Refactor purge script

--- scripts/purge.sh
+++ scripts/purge.sh
@@ -1,27 +1,36 @@
 #!/usr/bin/env bash
 set -e
 
-BOLD_START="\033[1m"
-BOLD_END="\033[0m"
-RELATIVE_PATH_FROM_SCRIPT_DIR="../"
 
-SCRIPT_DIR_PATH="${BASH_SOURCE:0:-8}"
-cd "$SCRIPT_DIR_PATH"
 
-# Set working location to the project root directory
-printf "\n${BOLD_START} 1. Setting script working location...${BOLD_END}\n"
+# Base
+SCRIPT_FILE_NAME="purge.sh"
+SCRIPT_DIR_PATH="${BASH_SOURCE:0:-${#SCRIPT_FILE_NAME}}"
+source "${SCRIPT_DIR_PATH}common.sh"
+check_root
 
-cd "${RELATIVE_PATH_FROM_SCRIPT_DIR}"
+
+
+# Set working location to project root directory
+print_step "Setting working location to project root directory"
+
+cd_to_project_root "$SCRIPT_DIR_PATH"
+
+
 
 # Remove python 3 virtual environment
-printf "\n${BOLD_START} 2. Remove python 3 virtual environment...${BOLD_END}\n"
+print_step "Removing python 3 virtual environment"
 
 sudo rm -rf ./scooter-control/.venv/
 
+
+
 # Remove python 3 cache directories
-printf "\n${BOLD_START} 3. Remove python 3 cache...${BOLD_END}\n"
+print_step "Removing python 3 cache"
 
 find ./scooter-control/ | grep -E "(/__pycache__$|\.pyc$|\.pyo$)" | xargs rm -rf
 
+
+
 # Finishing
-printf "\n${BOLD_START} 4. Finishing...${BOLD_END}\n\n"
+print_step "Finishing" "1:2"
