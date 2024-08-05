a555e909b46f0254bae4d625dea66e20cf90e47e
Aubrey ROUET <aubrey.rouet@gmail.com>
2024-08-06 01:17:19 +0200

    Fix launch script

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -29,7 +29,7 @@ targets=("Scooter Control")
 while IFS= read -r file; do
     test_target_file_basename="$(basename "${file}")"
     targets+=("${test_target_file_basename:0:-3}")
-done < <(find "./scooter-control/test/" -maxdepth 1 -type f -name "*.py" 2>/dev/null)
+done < <(find "./scooter-control/tests/" -maxdepth 1 -type f -name "*.py" 2>/dev/null)
 
 
 
@@ -45,15 +45,15 @@ selected_target_index=$(<"$temp_file")
 exec 3>&-
 
 if [[ "${selected_target_index}" -eq 1 ]]; then
-    target_path="scooter-control/main.py"
+    target_path="scooter-control.main"
 else
     target="${targets[$((selected_target_index - 1))]}"
-    target_path="scooter-control/test/${target}.py"
+    target_path="scooter-control.tests.${target}"
 fi
 
 printf "\n"
 
-if ! ./scooter-control/.venv/bin/python3 "${target_path}"; then
+if ! ./scooter-control/.venv/bin/python3 -m "${target_path}"; then
     print_bold "\nFailed to launch the program.\n"
 
     print_exit_step_and_exit
