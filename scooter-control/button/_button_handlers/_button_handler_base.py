9f363c169ba7ec15210ae30357b3b02f05cb26db
Aubrey ROUET <aubrey.rouet@gmail.com>
2024-08-05 16:28:01 +0200

    Add on_enable and on_disable events on ButtonHandlerBase

--- scooter-control/button/_button_handlers/_button_handler_base.py
+++ scooter-control/button/_button_handlers/_button_handler_base.py
@@ -8,16 +8,24 @@ class ButtonHandlerBase(ABC):
 
     def enable(self) -> None:
         self._enabled = True
+        self.on_enable()
     def disable(self) -> None:
         self._enabled = False
+        self.on_disable()
     @property
     def enabled(self) -> bool:
         return self._enabled
+    
+    @abstractmethod
+    def on_enable(self) -> None:
+        pass
+    @abstractmethod
+    def on_disable(self) -> None:
+        pass
 
     @abstractmethod
     def on_value_raised(self) -> None:
         pass
-
     @abstractmethod
     def on_value_recessed(self) -> None:
         pass
