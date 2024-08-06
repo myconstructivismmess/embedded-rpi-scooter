03aba7b6a4299a60af28e2249ddde78fa0833380
Aubrey ROUET <aubrey.rouet@gmail.com>
2024-08-07 01:19:59 +0200

    Fix Boolean Class

--- scooter-control/boolean.py
+++ scooter-control/boolean.py
@@ -1,15 +1,23 @@
-class Boolean(bool):
+# Main
+class Boolean:
     def __init__(self, value: object):
         if isinstance(value, str):
             if value.lower() in ("true", "1"):
-                super().__init__(True)
+                self._value: bool = True
             elif value.lower() in ("false", "0"):
-                super().__init__(False)
+                self._value: bool = False
             else:
                 raise ValueError(f"Invalid string value: {value}")
         elif isinstance(value, int):
-            super().__init__(value != 0)
+            self._value: bool = value != 0
         elif isinstance(value, None):
             raise ValueError("None is not a valid boolean value")
         else:
-            super().__init__(value)
+            self._value: bool = bool(value)
+
+    def __bool__(self) -> bool:
+        return self._value
+    
+    @property
+    def value(self) -> bool:
+        return self._value
