875848ad353f79eb7a107c7acb93ada6ac757571
Aubrey ROUET <aubrey.rouet@gmail.com>
2024-08-06 03:07:49 +0200

    Fix array and button utils imports, renaming their files and clean __init__.py in utils.

--- scooter-control/utils/button.py
+++ scooter-control/utils/button.py
@@ -1,14 +1,12 @@
-# Packages Imports
-from microcontroller import Pin
-
 # Project Imports
-from Button import ButtonBuilder
+from ..button import ButtonBuilder
+from ..gpio_pin import GPIOPin
 
 # Main
 def add_keyboard_interceptor_if_enabled(builder: ButtonBuilder, debug_key_name: str, enabled: bool = False, reverse_signal: bool = False) -> None:    
     if enabled:
         builder.add_keyboard_interceptor(debug_key_name, reverse_signal)
 
-def add_gpio_pin_interceptor_if_enabled(builder: ButtonBuilder, gpio_pin: Pin, enabled: bool = False, reverse_signal: bool = False) -> None:
+def add_gpio_pin_interceptor_if_enabled(builder: ButtonBuilder, gpio_pin: GPIOPin, enabled: bool = False, reverse_signal: bool = False) -> None:
     if enabled:
         builder.add_gpio_pin_interceptor(gpio_pin, reverse_signal)
