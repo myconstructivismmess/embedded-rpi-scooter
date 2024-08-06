b518859b6948e00f7bd68a8785540bdebd8d2ffa
Aubrey ROUET <aubrey.rouet@gmail.com>
2024-08-06 18:22:11 +0200

    Add an env utils

--- scooter-control/utils/__init__.py
+++ scooter-control/utils/__init__.py
@@ -1,4 +1,5 @@
 __all__ = (
     'array',
     'button',
+    'env',
 )
---
+++ scooter-control/utils/env.py
@@ -0,0 +1,26 @@
+# Python Imports
+import os
+from sys import exit
+import logging
+
+# Packages Imports
+from dotenv import load_dotenv
+
+# Main
+def load_env_file(script_path: str, relative_path_to_env_directory: str) -> None:
+    """
+    Load environment variables from a file and exit if environment file cannot be loaded.
+    """
+    script_dir_path = os.path.dirname(script_path)
+    env_dir_path = os.path.join(script_dir_path, relative_path_to_env_directory)
+    file_path = os.path.join(env_dir_path, ".env")
+
+    if not os.path.exists(file_path):
+        logging.error(f"Environment file not found: {file_path}")
+        exit(1)
+
+    if load_dotenv(dotenv_path=file_path):
+        logging.info(f"Loaded environment variables from {file_path}")
+    else:
+        logging.error(f"Failed to load environment variables from {file_path}")
+        exit(1)
