7ae5ea24b48f886408f5ef22c10732c543018a7b
Aubrey ROUET <aubrey.rouet@gmail.com>
2024-08-08 18:56:24 +0200

    Added SoundsLoadedResultMessage to sound thread messages and enhance logging clarity

--- scooter-control/horn_sound_player/horn_sound_player_thread_messages.py
+++ scooter-control/horn_sound_player/horn_sound_player_thread_messages.py
@@ -1,15 +1,17 @@
 # Python Imports
-from enum import Enum
-
 import logging
+
+from enum import Enum
+from typing import Dict
 
 # Main
 class MessageType(Enum):
-    START_SOUND = 1
-    STOP_REPEATABLE_SOUND = 2
-    LOG = 3
-    WARNING = 4
-    ERROR = 5
+    SOUNDS_LOADED_RESULT = 1
+    START_SOUND = 2
+    STOP_REPEATABLE_SOUND = 3
+    LOG = 4
+    WARNING = 5
+    ERROR = 6
 
 class Message:
     def __init__(self, type: MessageType) -> None:
@@ -18,7 +20,18 @@ class Message:
     @property
     def type(self) -> MessageType:
         return self._type
+    
+class SoundsLoadedResultMessage(Message):
+    def __init__(self, sounds_load_success_status: Dict[int, bool]) -> None:
+        super().__init__(MessageType.SOUNDS_LOADED_RESULT)
+        self._sounds_load_success_status: Dict[int, bool] = sounds_load_success_status
 
+    def is_sound_loaded(self, index: int) -> bool:
+        return self._sounds_load_success_status.get(index, False)
+    
+    @property
+    def sounds_load_success_status(self) -> Dict[int, bool]:
+        return self._sounds_load_success_status.copy()
 
 class StartSoundMessage(Message):
     def __init__(self, index: int) -> None:
@@ -40,7 +53,7 @@ class LogThreadMessage(Message):
         self._log_message: str = log_message
 
     def log(self) -> None:
-        logging.info(self._log_message)
+        logging.info(f"horn_sound_player: {self._log_message}")
 
 class WarningThreadMessage(Message):
     def __init__(self, warning_message: str) -> None:
@@ -48,7 +61,7 @@ class WarningThreadMessage(Message):
         self._warning_message: str = warning_message
 
     def log(self) -> None:
-        logging.warning(self._warning_message)
+        logging.warning(f"horn_sound_player: {self._warning_message}")
 
 class ErrorThreadMessage(Message):
     def __init__(self, exception: Exception) -> None:
@@ -56,4 +69,4 @@ class ErrorThreadMessage(Message):
         self._exception: Exception = exception
 
     def log(self) -> None:
-        logging.error(self._exception)
+        logging.error(f"horn_sound_player: {self._exception}")
